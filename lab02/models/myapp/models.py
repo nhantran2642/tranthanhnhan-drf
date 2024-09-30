9e50ccb4be4fd6209eefaa1edf4848bb0b4c7072
Nhan Brown <ttnhan2642@gmail.com>
2024-09-30 21:34:09 +0700

    Models

---
+++ lab02/models/myapp/models.py
@@ -0,0 +1,126 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+
+    def baby_boomer_status(self):
+        "Returns the person's baby-boomer status."
+        import datetime
+
+        if self.birth_date < datetime.date(1945, 8, 1):
+            return "Pre-boomer"
+        elif self.birth_date < datetime.date(1965, 1, 1):
+            return "Baby boomer"
+        else:
+            return "Post-boomer"
+
+    @property
+    def full_name(self):
+        "Returns the person's full name."
+        return f"{self.first_name} {self.last_name}"
+
+
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
+    
+class Runner(models.Model):
+    MedalType = models.TextChoices("MedalType", "GOLD SILVER BRONZE")
+    name = models.CharField(max_length=60)
+    medal = models.CharField(blank=True, choices=MedalType, max_length=10)
+    
+class Fruit(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+    
+class Manufacturer(models.Model):
+    pass
+
+
+class Car(models.Model):
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through="Membership")
+
+    def __str__(self):
+        return self.name
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
+    
+class Ox(models.Model):
+    horn_length = models.IntegerField()
+
+    class Meta:
+        ordering = ["horn_length"]
+        verbose_name_plural = "oxen"
+        
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.TextField()
+
+    def save(self, **kwargs):
+        self.slug = slugify(self.name)
+        if (
+            update_fields := kwargs.get("update_fields")
+        ) is not None and "name" in update_fields:
+            kwargs["update_fields"] = {"slug"}.union(update_fields)
+        super().save(**kwargs)
+        
+class CommonInfo(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+
+    class Meta:
+        abstract = True
+        ordering = ["name"]
+
+
+class Unmanaged(models.Model):
+    class Meta:
+        abstract = True
+        managed = False
+
+
+class Student(CommonInfo, Unmanaged):
+    home_group = models.CharField(max_length=5)
+
+    class Meta(CommonInfo.Meta, Unmanaged.Meta):
+        pass
+    
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+
+class Restaurant(Place):
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+    
+class MyPerson(Person):
+    class Meta:
+        proxy = True
+
+    def do_something(self):
+        pass
+    
+class OrderedPerson(Person):
+    class Meta:
+        ordering = ["last_name"]
+        proxy = True
